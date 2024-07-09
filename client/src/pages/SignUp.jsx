46926b3530e7070f136ae793e1cb7d93d9c5dd8f
Rizwana <rizwanarizz1223@gmail.com>
2024-07-09 17:35:03 +0530

    one hour fourty min and twenty seconds

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id] : e.target.value});
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch ('http://localhost:3000/api/auth/signup', formData);
+    const data = await res.json();
+    console.log(data);
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
 
         <input
@@ -17,6 +29,7 @@ export default function SignUp() {
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
 
         <input
@@ -24,6 +37,7 @@ export default function SignUp() {
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
 
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
