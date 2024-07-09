88be4814078a8c200c08ad9e78e14623ed20ebdb
Rizwana <rizwanarizz1223@gmail.com>
2024-07-09 14:23:00 +0530

    created user modal

--- api/index.js
+++ api/index.js
@@ -1,18 +1,19 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log('connected to mongoDB');
-})
-.catch((err) => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log('server listening on server 3000');
+app.listen(3000, () => {
+  console.log("server listening on server 3000");
 });
